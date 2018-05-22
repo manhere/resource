2f203cce286057fea1cfab4f26bbe86f3743a770
TujiaWang <wangguangxi88@gmail.com>
2018-05-23 00:15:35 +0800

    modif file

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 //app.js
 App({
-  onLaunch: function () {
-  },
-  globalData: {
-    userInfo: null
-  }
+    onLaunch: function () {
+        var self = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                self.globalData.wh = res.windowHeight;
+            }
+        })
+    },
+    globalData: {
+        wh:0,
+        requestUrl: 'https://t.lovezyd.com/Api/Index'
+    },
+    wxLogin: function (encryptedData,iv,fun) {
+        var self = this;
+        wx.login({
+            success: function (res) {
+                console.log(res)
+                if (res.code) {
+                    wx.request({
+                        url: self.globalData.requestUrl + '/wxLogin',
+                        method: 'POST',
+                        header: { 'content-type': 'application/x-www-form-urlencoded' },
+                        data: {
+                            code: res.code,
+                            encryptedData: encryptedData,
+                            iv:iv
+                        },
+                        success:function(result){
+                            wx.setStorageSync('session_id', result.data.session_id)
+                            fun();
+                            console.log(result)
+                        }
+                    })
+                } else {
+                    self.wxLogin();
+                }
+            }
+        });
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,16 +5,79 @@ Page({
     data: {
         sms: '',
         smsList: [],
-        toView:'smsl0',
-        wh:0
+        toView: 'smsl0',
+        wh: 0,
+        auth: false,
+        userInfo: {},
+        keywords: ''
     },
     onLoad: function () {
+        this.setData({
+            wh: app.globalData.wh
+        });
         var self = this;
-        wx.getSystemInfo({
+        if (wx.getStorageSync('userInfo')) {
+            this.setData({
+                userInfo: wx.getStorageSync('userInfo')
+            });
+            
+        } else {
+            this.getUserInfo(function () { });
+        }
+        this.getWelcomeSms();
+    },
+    getUserInfo: function (fun) {
+        var self = this;
+        wx.getUserInfo({
             success: function (res) {
-                self.setData({
-                    wh: res.windowHeight
+                console.log(res)
+                wx.setStorageSync('userInfo', res.userInfo)
+                app.wxLogin(res.encryptedData, res.iv, function () {
+                    self.setData({
+                        auth: false
+                    });
+                    fun();
                 });
+            },
+            fail: function (err) {
+                self.setData({
+                    auth: true
+                });
+            }
+        })
+    },
+    getWelcomeSms: function () {
+        var self = this;
+        wx.request({
+            url: app.globalData.requestUrl + '/getWelcome',
+            method: 'POST',
+            header: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: {
+                sessionId: wx.getStorageSync('session_id')
+            },
+            success: function (result) {
+                console.log(result)
+                if (result.data.status == 2) {
+                    self.getUserInfo(function () {
+                        self.getWelcomeSms();
+                    });
+                } else {
+                    var tmp = [];
+                    tmp.push({
+                        txt: result.data.data.title1, tpy: 'others'
+                    });
+                    tmp.push({
+                        txt: result.data.data.title2, tpy: 'others'
+                    });
+                    tmp.push({
+                        txt: result.data.data.title3, tpy: 'others'
+                    });
+                    console.log(tmp)
+                    self.setData({
+                        loading: false,
+                        smsList: tmp
+                    });
+                }
             }
         })
     },
@@ -25,27 +88,73 @@ Page({
     },
     sendSms: function () {
         var tmp = this.data.smsList;
-        tmp.push({ txt: this.data.sms,tpy:'mysms'});
+        tmp.push({ txt: this.data.sms, tpy: 'mysms' });
         this.setData({
-            smsList:tmp,
-            sms:''
+            smsList: tmp,
+            sms: '',
+            keywords: this.data.sms
         });
         this.getResSms();
-        if (this.data.smsList.length >= 11){
+        if (this.data.smsList.length >= 6) {
             this.setData({
                 toView: 'sms' + (this.data.smsList.length - 1)
             });
         }
     },
-    getResSms:function(){
+    getResSms: function () {
         var tmp = this.data.smsList;
-        tmp.push({ txt: Math.random(), tpy: 'others' });
+        // tmp.push({ txt: Math.random(), tpy: 'others' });
+        tmp.push({ txt: '', tpy: 'others', isload: true });
         this.setData({
-            smsList:tmp
+            smsList: tmp
         });
-        if (this.data.smsList.length >= 11) {
+        if (this.data.smsList.length >= 6) {
             this.setData({
                 toView: 'sms' + (this.data.smsList.length - 1)
+            });
+        }
+        var self = this;
+
+        wx.request({
+            url: app.globalData.requestUrl + '/getContent',
+            method: 'POST',
+            header: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: {
+                sessionId: wx.getStorageSync('session_id'),
+                keyWords: self.data.keywords
+            },
+            success: function (result) {
+                console.log(result)
+                if (result.data.status == 2) {
+                    self.getUserInfo(function () {
+                        self.getResSms();
+                    });
+                } else {
+                    setTimeout(function () {
+                        if (result.data.status == 0) {
+                            tmp[tmp.length - 1].txt = result.data.info;
+                        } else {
+                            tmp[tmp.length - 1].txt = result.data.data;
+                        }
+                        tmp[tmp.length - 1].isload = false;
+                        self.setData({
+                            smsList: tmp
+                        });
+                    }, 1000);
+                }
+            }
+        })
+    },
+    getUserInfos: function (e) {
+        console.log(e)
+        var self = this;
+        if (e.detail.userInfo) {
+            wx.setStorageSync('userInfo', e.detail.userInfo)
+            app.wxLogin(e.detail.encryptedData, e.detail.iv, function () {
+                self.setData({
+                    auth: false
+                });
+                self.onLoad();
             });
         }
     }
